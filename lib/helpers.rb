4d7690712532dedbb215a259e531abb55ef56332
Damien Pollet <damien.pollet@gmail.com>
2012-10-12 17:30:50 +0200

    Helper for formatting phone numbers

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -2,3 +2,14 @@ def clear
   '<div class="clear">&nbsp;</div>'
 end
 
+def phone(main, opts={})
+  prefix = opts.fetch :prefix, 33
+  separator = opts[:separator] || '&nbsp;'
+
+  parts = main.scan /\d+/
+  unless prefix.nil?
+    parts[0].sub!(/\d/) { |digit| "(#{digit})" }
+    parts.unshift "+#{prefix}"
+  end
+  parts.join separator
+end
