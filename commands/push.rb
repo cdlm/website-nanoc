8cccba4767c1377522408635a2cb6cef89f2fbb9
Damien Pollet <damien.pollet@gmail.com>
2012-10-24 03:55:28 +0200

    Rely on auto-pruning to simplify git push deploy
    - output to a subdir of the pushed repo to avoid rsync local deploy
    - check for changes, log messages

--- commands/push.rb
+++ commands/push.rb
@@ -4,15 +4,18 @@ aliases :gh, :'gh-pages'
 summary 'deploy to github pages'
 
 run do |opts, args, cmd|
-  # sync output to the pages repo
-  Nanoc::CLI.run %w{deploy -t default}
-
   require 'git'
   git = Git.open 'staging'
   begin
-    git.add
-    git.commit "Deploy on #{Time.now.to_s}", add_all: true
-    git.push
+    touched = [git.status.added, git.status.changed, git.status.deleted, git.status.untracked]
+    if touched.all? { |each| each.empty? }
+        Nanoc::CLI::Logger.instance.log(:low, "No changes to push")
+    else
+      git.add
+      git.commit "Deploy on #{Time.now.to_s}", add_all: true
+      Nanoc::CLI::Logger.instance.log(:low, "Pushing deploy commit #{git.revparse(git.current_branch)}…")
+      git.push
+    end
   rescue Git::GitExecuteError => e
     puts e.message
   end
